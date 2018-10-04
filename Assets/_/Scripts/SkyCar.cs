6ebf63716e018fff975b0d04e4992971d362ead0
Jepe <unityjepe@gmail.com>
2018-10-04 10:30:43 +0200

    Initial commit

---
+++ Assets/_/Scripts/SkyCar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyCar : MonoBehaviour {
+
+	[SerializeField] int speed;
+	public Transform target;
+
+	void Start () {
+
+		speed = 10;
+	}
+	
+	void Update () {
+
+		transform.position += Vector3.forward * Time.deltaTime * speed;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Target")
+		{
+			transform.position += Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Target")
+		{
+			speed = 0;
+		}
+	}
+}
